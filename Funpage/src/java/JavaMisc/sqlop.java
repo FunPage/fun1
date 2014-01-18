ce8d70e7eee655a523a7c9ab6787f948c50d128e
plasmashadow <plasmashadowx@gmail.com>
2014-01-18 09:23:24 +0530

    commit all.............

--- Funpage/src/java/JavaMisc/sqlop.java
+++ Funpage/src/java/JavaMisc/sqlop.java
@@ -10,7 +10,7 @@ import javax.servlet.*;
  * @author sathyagriffin
  */
 public class sqlop {
-    
+    //this is for context initialisation
     public void CreateTable(String tablename)
     {
    try
@@ -25,6 +25,52 @@ public class sqlop {
     {
         System.out.println(e);
     }
+    }
+    //this is for login
+    public ResultSet Returnset(String email,String password)
+    {  ResultSet rs = null;
+        try
+        {
+        
+        Class.forName("com.mysql.jdbc.Driver");
+        String Url="jdbc:mysql://localhost:3306/SGC";
+        Connection con=DriverManager.getConnection(Url,"root","root");
+        Statement stmt=con.createStatement();
+        rs=stmt.executeQuery("select * from Users where email="+email+" and password="+password);
+
+        }
+        catch(Exception e)
+        {
+        System.out.println(e);
+        }
+        return rs;
+    }
+    
+    //this is for sign up.
+    public boolean InsertTable(String firstname,String lastname,String profilename,String email,String password,String DOB)
+    {
+        int condition = 0;
+    try
+    {   String sql="Insert into Users values(?,?,?,?,?,?)";
+        Class.forName("com.mysql.jdbc.Driver");
+        String Url="jdbc:mysql://localhost:3306/SGC";
+        Connection con=DriverManager.getConnection(Url,"root","root");
+        PreparedStatement stmt=con.prepareStatement(sql);
+        stmt.setString(1,firstname);
+        stmt.setString(2, lastname);
+        stmt.setString(3, profilename);
+        stmt.setString(4, email);
+        stmt.setString(5, password);
+        stmt.setString(6, DOB);
+       condition=stmt.executeUpdate();
+        
+    }
+    catch(Exception e)
+    {System.out.println(e);}
+    if(condition>0)
+    {return true;}
+    else
+    {return false;}
     }
     
 }
--- Funpage/src/java/sgc/SignupController.java
+++ Funpage/src/java/sgc/SignupController.java
@@ -4,7 +4,7 @@ package sgc;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import JavaMisc.sqlop;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -34,9 +34,34 @@ public class SignupController extends HttpServlet {
             throws ServletException, IOException {
        try
        {
+          //boolean condition;
+        sqlop s=new sqlop();
+        String firstname=request.getParameter("firstname");
+        String lastname=request.getParameter("lastname");
+        String profilename=request.getParameter("profilename");
+        String emailid=request.getParameter("email");
+        String password=request.getParameter("password0");
+        String DOB=request.getParameter("DOB");
         response.setContentType("text/plain");
         PrintWriter out=response.getWriter();
-        
+       if(s.InsertTable(firstname, lastname, profilename, emailid, password, DOB))
+        {response.sendRedirect("welcome.jsp");}
+        else
+        {response.sendRedirect("MiscWebPages/SignUpincomplete.html");}
+        //condition=profilename.isEmpty() && emailid.isEmpty() && password.isEmpty() && firstname.isEmpty() && lastname.isEmpty() && DOB.isEmpty();
+       // System.out.println(condition);
+        /*if(condition)
+        {
+        response.sendRedirect("SignUpincomplete.html");
+        out.println("failed signup");
+        }
+        else
+        {
+        if(s.InsertTable(firstname, lastname, profilename, emailid, password, DOB))
+        {out.println("signup successfull");}
+        else
+        {out.println("failed signup");}
+        }*/
        }
        catch(Exception e){System.out.println(e);}
     }
